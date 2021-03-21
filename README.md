5a4691a19fa2ef5f43408ff12c73399d7b95879d
Blake Steel <blakemsteel@gmail.com>
2021-03-21 14:07:35 -0400

    checking username, password, and token

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 go-onecause
+
+resources
+
+https://tutorialedge.net/golang/creating-restful-api-with-golang/
+https://auth0.com/blog/authentication-in-golang/
+
+login info:
+https://theshiva5.medium.com/creating-simple-login-api-using-go-and-mongodb-9b3c1c775d2f
+
+returning errors:
+https://medium.com/@ozdemir.zynl/rest-api-error-handling-in-go-behavioral-type-assertion-509d93636afd
+
+getting date in go:
+https://stackoverflow.com/questions/18627415/go-printing-date-to-console/53404116
+
+formatting string:
+https://stackoverflow.com/questions/11123865/format-a-go-string-without-printing
--- main.go
+++ main.go
@@ -1,21 +1,79 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
+
+type LoginFormat struct {
+	Username string `json:"Username"`
+	Password string `json:"Password"`
+}
+
+var User LoginFormat
+
+func main() {
+	User = LoginFormat{
+		Username: "c137@onecause.com",
+		Password: "#th@nH@rm#y#r!$100%D0p#",
+	}
+	handleRequests()
+}
+
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homePage)
+	router.HandleFunc("/auth", authUser).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
+func authUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user = User
+	reqBody, _ := ioutil.ReadAll(r.Body)
 
-func main() {
-	handleRequests()
+	err := json.Unmarshal(reqBody, &user)
+	if err != nil {
+		log.Printf("Body parse error, %v", err)
+		w.WriteHeader(400)
+		return
+	}
+
+	tokenString := r.Header.Get("Authorization")
+	hour, minute, _ := time.Now().UTC().Clock()
+	correctToken := fmt.Sprintf("%02d%02d", hour, minute)
+	fmt.Printf("%s, %s\n", tokenString, correctToken)
+	if tokenString != correctToken {
+		log.Printf("Token incorrect")
+		w.WriteHeader(401)
+		return
+	}
+
+
+	if user.Username != User.Username {
+		log.Printf("Username incorrect")
+		w.WriteHeader(401)
+		return
+	}
+
+	if user.Password != User.Password {
+		log.Printf("Password incorrect")
+		w.WriteHeader(401)
+		return
+	}
+
+
+
+	json.NewEncoder(w).Encode(User)
 }
